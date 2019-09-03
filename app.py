3f1b18637e1441ff6eb5276f221ff03f7a078802
unknown <7602811+TanGolf@users.noreply.github.com>
2019-09-03 21:28:48 +0100

    Minor reshuffling of code to ensure db is created to cater for uwsgi error

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='lorax' #This should never be shared publicly and is for illustration purposes only.
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt  = JWT(app, authenticate, identity)
 

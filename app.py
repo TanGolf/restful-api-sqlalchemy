cd67bd597eaf12fcef85613625028f5edf957070
unknown <7602811+TanGolf@users.noreply.github.com>
2019-09-03 21:46:31 +0100

    Corrected error in run.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import  Api
 from flask_jwt import JWT
@@ -7,7 +8,7 @@ from resources.item import Item, ItemList
 from security import authenticate, identity
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='lorax' #This should never be shared publicly and is for illustration purposes only.
 api = Api(app)
